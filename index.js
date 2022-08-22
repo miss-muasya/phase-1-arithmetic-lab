d867927b4b3826c97663a234e909cce3cf84efbe
miss-muasya <sheemuasya@gmail.com>
2022-08-22 12:53:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1
+let multiply = num1*num2
+let random = Math.floor(Math.random()*10)+1;
+let num3=14
+let num4=10
+let mod =num3%num4
+let max =Math.max(20,6,4,3)
+console.log(max);
